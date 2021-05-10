2e3061385418cd63a6acb8f2c254f1698261a131
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-05-09 21:50:34 -0300

    Home, Navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,30 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Página home</h1>
-        </div>
+        <>
+            <NavBar />
+
+            <div className="container py-5">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acessar Dashboard
+                    </Link>
+                </div>
+            </div>
+
+            <br /><br />
+
+            <Footer />
+        </>
     );
 };
-  
+
 export default Home;
-  
